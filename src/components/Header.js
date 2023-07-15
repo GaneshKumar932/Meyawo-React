24e340a4d6046999b7777bdef9cc96ca8fccfb67
Ganesh Kumar <gk@9989915@gmail.com>
2023-07-15 20:19:21 +0530

    changed some files and its data

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,36 +3,36 @@ import { Link } from 'react-router-dom/dist';
 
 const Header = () => {
   
-    // document.addEventListener("DOMContentLoaded", function() {
-    //     var navbar = document.querySelector(".navbar");
-    //     navbar.addEventListener('click', function(event) {
-    //         if (this.hash !== "") {
-    //             event.preventDefault();
-    //             var hash = this.hash;
-    //             var targetElement = document.querySelector(hash);
-    //             var targetOffsetTop = targetElement.offsetTop;
+    document.addEventListener("DOMContentLoaded", function() {
+        var navbar = document.querySelector(".navbar");
+        navbar.addEventListener('click', function(event) {
+            if (this.hash !== "") {
+                event.preventDefault();
+                var hash = this.hash;
+                var targetElement = document.querySelector(hash);
+                var targetOffsetTop = targetElement.offsetTop;
     
-    //             window.scrollTo({
-    //                 top: targetOffsetTop,
-    //                 behavior: 'smooth'
-    //             });
+                window.scrollTo({
+                    top: targetOffsetTop,
+                    behavior: 'smooth'
+                });
     
-    //             window.location.hash = hash;
-    //         }
-    //     });
+                window.location.hash = hash;
+            }
+        });
     
-    //     var navToggle = document.getElementById("nav-toggle");
-    //     navToggle.addEventListener('click', function() {
-    //         this.classList.toggle('is-active');
-    //         var navList = document.querySelector('ul.nav');
-    //         navList.classList.toggle('show');
-    //         console.log("hi");
-    //     });
-    // });
+        var navToggle = document.getElementById("nav-toggle");
+        navToggle.addEventListener('click', function() {
+            this.classList.toggle('is-active');
+            var navList = document.querySelector('ul.nav');
+            navList.classList.toggle('show');
+            console.log("hi");
+        });
+    });
     
   return (
     <div>
- <nav className="custom-navbar affix affix-top"  data-spy="affix" data-offset-top="20">
+ <nav className="custom-navbar affix-top"  data-spy="affix" data-offset-top="20">
     <div className="container">
         <a className="logo" href="#">Meyawo</a>         
         <ul className="nav">
